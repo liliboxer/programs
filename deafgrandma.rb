0f6d58d629ee9b9e9ba328a8c454a138973e0c99
Lili Boxer <ninaliboxer@gmail.com>
2017-04-20 13:24:09 -0600

    Updated loop with a while loop

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -1,7 +1,26 @@
-response = gets.chomp
+bye = 0
 
-if response != response.upcase
-  puts "HUH?! SPEAK UP, SONNY!"
-elsif response = response.upcase
-    puts "NO, NOT SINCE 1938!"
+while bye <3
+  response = gets.chomp
+
+  if response == "BYE"
+    bye += 1
+    if bye <3
+      puts "I THOUGHT THE SAME THING"
+    end
+
+  elsif response = response.upcase
+    number = rand(1930...1950)
+    puts "NO! NOT SINCE #{number}"
+    bye = 0
+
+  else
+    puts "HUH?! SPEAK UP SONNY!"
+    bye = 0
+  end
+
+  if bye == 3
+    puts "I'M SAD TO SEE YOU GO"
+  end
+
 end
