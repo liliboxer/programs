df5789ac59013d4afcaa1aa927973347a6aedb50
Lili Boxer <ninaliboxer@gmail.com>
2017-04-20 15:34:03 -0600

    It works

--- leapyear.rb
+++ leapyear.rb
@@ -4,9 +4,8 @@ puts "Type another year "
 b = gets.chomp.to_i
 
 while a <= b
-  if a % 4 == 0 || a % 100 != 0 || a % 400 ==0
+  if a % 4 == 0 && a % 100 != 0 || a % 400 == 0
       puts a
-    end
   end
     a += 1
 end
