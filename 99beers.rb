2ea869ec16dba9052954e95d67eb8a0d46be2c10
Lili Boxer <ninaliboxer@gmail.com>
2017-04-23 16:19:28 -0600

    updated

--- 99beers.rb
+++ 99beers.rb
@@ -10,7 +10,4 @@ while x > y
   puts "#{x} bottles of beer on the wall, #{x} bottles of beer! "
   x -= 1
   puts "Take one down, pass it around, #{x} bottles of beer on the wall! "
-  if x == y
-    break
-  end
 end
