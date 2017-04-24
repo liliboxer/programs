d998c7c62fad89a2644aabd96dbc0565741e11cc
Lili Boxer <ninaliboxer@gmail.com>
2017-04-24 14:19:16 -0600

    max game

--- max.rb
+++ max.rb
@@ -2,13 +2,37 @@ time = 8
 biscuits = 20
 
 while biscuits < 100
-  if threat = "oranges"
-    20/hr
-  elsif
-    50% chance dropping tray
-    biscuit -= 20
+  puts "Threaten Max with oranges? "
+  threat = gets.chomp
+  if threat == "yes"
+    if rand(1..100) >= 50
+      biscuits -= 20
+      puts "Max dropped the tray! "
+    else
+        biscuits += 20
+        puts "Max made 20 biscuits this hour! "
+    end
   else
-
-
+    if rand(1..100) <= 25
+      biscuits -= 15
+      puts "Max dropped his tray! "
+    else
+      biscuits += 15
+      puts "Max made 15 biscuits this hour! "
+    end
+  end
+  time -= 1
+  if time == 0
+    puts "You're fired Max!"
+    break
+  elsif biscuits <= 0
+    puts "You're fired Max! "
+    break
+  elsif biscuits >= 100
+    puts "You did it Max! Final biscuit count: #{biscuits}"
+  else
+    puts "Max's biscuit count is at: #{biscuits} "
+    puts "Time remaining: #{time}"
+    puts "Will he get fired? "
   end
 end
