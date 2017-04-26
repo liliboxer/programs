68fad9fc8f66214dec481e368088e5cee254c54d
Lili Boxer <ninaliboxer@gmail.com>
2017-04-26 12:29:33 -0600

    addition

---
+++ met.rb
@@ -0,0 +1,27 @@
+def ask question
+while true
+  puts question
+  input = gets.chomp
+  if input == "yes" || input == "no"
+    puts " "
+    if input == "yes"
+      answer = true
+    else
+      answer = false
+    end
+    break
+  elsif
+    puts "Please answer yes or no"
+  end
+end
+end
+
+ask "Do you like tacos "
+ask "Do you like burritos? "
+ask "Do you wet the bed? "
+ask "Do you like eating chimichangas? "
+puts "Just a few more questions..."
+ask "Do you like drinking horchata? "
+ask "Do you like eating flautas? "
+
+puts "Thank you for answering the questions! "
