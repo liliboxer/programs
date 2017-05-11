1080a73854d84c829cb8f369e40415ad6344550f
Lili Boxer <ninaliboxer@gmail.com>
2017-05-11 15:00:08 -0600

    FUCL YEAH

--- each.rb
+++ each.rb
@@ -1,7 +1,11 @@
 form = ["what is your name?","how old are you? ","what city do you live in? "]
-
+answers = []
 
 form.each do |info|
   puts "#{info}"
   input = gets.chomp
+  answers.push(input)
 end
+
+puts 
+puts "Hi #{answers[0]}. You are #{answers[1]} years old and live in #{answers[2]}."
