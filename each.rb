a5ebf0d4ac5c35e9c4e8498eb197c6700db7a9ca
Lili Boxer <ninaliboxer@gmail.com>
2017-05-11 15:07:43 -0600

    capitalization

--- each.rb
+++ each.rb
@@ -3,9 +3,9 @@ answers = []
 
 form.each do |info|
   puts "#{info}"
-  input = gets.chomp
+  input = gets.chomp.capitalize 
   answers.push(input)
 end
 
-puts 
+puts
 puts "Hi #{answers[0]}. You are #{answers[1]} years old and live in #{answers[2]}."
